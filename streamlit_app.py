8df270578dfc77031e7ed4375b35b9d94037bf1f
preety <71422460+preety311@users.noreply.github.com>
2024-06-16 05:44:04 +0530

    Update streamlit_app.py
    Adding search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ st.write("The name in ur smoothie is: ", name_on_order)
 
 cnx=st.connection('snowflake')
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list= st.multiselect(
     "choose upto 5 ingredients: ", my_dataframe, max_selections=5
 )
